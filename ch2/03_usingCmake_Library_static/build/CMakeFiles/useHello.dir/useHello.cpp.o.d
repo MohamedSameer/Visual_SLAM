b2ac725f1421a95bc984b8017ddb32b807f712d4
Mohamed Sameer <dr.msameer@gmail.com>
2025-10-23 15:10:57 +0900

    mohamed commit =edit folders names commit

--- ch2/03_usingCmake_Library_static/build/CMakeFiles/useHello.dir/useHello.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/useHello.dir/useHello.cpp.o: \
- /media/mohamed/Transcend_2/3D\ Computer\ Vision/slambook2-master_my_Edit/ch2/03_usingCmake_Library/useHello.cpp \
- /usr/include/stdc-predef.h \
- /media/mohamed/Transcend_2/3D\ Computer\ Vision/slambook2-master_my_Edit/ch2/03_usingCmake_Library/libHelloSLAM.h
--- ch2/04_usingCmake_Library_shared/build/CMakeFiles/useHello.dir/useHello.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/useHello.dir/useHello.cpp.o: \
- /media/mohamed/Transcend_2/3D\ Computer\ Vision/slambook2-master_my_Edit/ch2/04_usingCmake_Library_shared/useHello.cpp \
- /usr/include/stdc-predef.h \
- /media/mohamed/Transcend_2/3D\ Computer\ Vision/slambook2-master_my_Edit/ch2/04_usingCmake_Library_shared/libHelloSLAM.h
